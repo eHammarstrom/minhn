fb12f0d3b46127c64b269167e1b839fa5e366b44
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2019-07-29 00:15:30 +0200

    fixed wrong fmemopen mode, and may now pass preallocated buffer to request func

--- main.c
+++ main.c
@@ -33,24 +33,27 @@ static size_t curl_write_cb(void *ptr, size_t size, size_t nmemb, void *data)
 	return fwrite(ptr, size, nmemb, (FILE *)data);
 }
 
-char *request_body(char *url)
+/* allocates bufsize if supplied buf is NULL */
+int request_body(char *buf, size_t bufsiz, char *url)
 {
 	CURL *curl;
 	CURLcode res;
-	char *buf;
 	FILE *http_buf;
 
-	buf = malloc(sizeof(char) * HTTP_BUFSIZ);
-	http_buf = fmemopen(buf, HTTP_BUFSIZ, "r+");
+	if (!buf) {
+		buf = malloc(bufsiz);
+	}
+
+	http_buf = fmemopen(buf, bufsiz, "w+");
 
 	if (!http_buf) {
-		FATAL("unable to allocate http buffer");
+		return false;
 	}
 
 	curl = curl_easy_init();
 
 	if (!curl) {
-		FATAL("unable to initialize CURL");
+		goto req_err;
 	}
 
 	curl_easy_setopt(curl, CURLOPT_URL, url);
@@ -62,12 +65,16 @@ char *request_body(char *url)
 	curl_easy_cleanup(curl);
 
 	if (res != CURLE_OK) {
-		FATAL("curl failed with response %d", res);
+		goto req_err;
 	}
 
+	/* success */
 	fclose(http_buf);
+	return true;
 
-	return buf;
+req_err:
+	fclose(http_buf);
+	return false;
 }
 
 size_t prettify_stories(char **str, json_t **stories)
@@ -92,8 +99,12 @@ size_t prettify_stories(char **str, json_t **stories)
 		url = json_string_value(jurl);
 		title = json_string_value(jtitle);
 
-		fprintf(f, "%d: %s", i + 1, title);
-		fprintf(f, "\t(%s)", url);
+		fprintf(f, "%d: %s\n", i + 1, title);
+
+		if (i + 1 == NUM_STORIES)
+			fprintf(f, "\t(%s)", url);
+		else
+			fprintf(f, "\t(%s)\n\n", url);
 
 		json_decref(jurl);
 		json_decref(jtitle);
@@ -111,16 +122,20 @@ int main(int argc, char *argv[])
 	bool help = false;
 	bool verbose = false;
 
-	char *stories_text;
-	char *story_text;
+	char stories_text[HTTP_BUFSIZ] = { 0 };
+	char story_text[HTTP_BUFSIZ] = { 0 };
 	char story_text_url[256] = { 0 };
 
 	json_error_t jerror;
 	json_t *stories_root;
 	json_t *stories[NUM_STORIES] = { NULL };
 
-	ITER_ARGS(HAS_ARG(help, "-h") HAS_ARG(help, "--help")
-			  HAS_ARG(verbose, "-v") HAS_ARG(verbose, "--verbose"));
+	ITER_ARGS(
+		HAS_ARG(help, "-h");
+		HAS_ARG(help, "--help");
+		HAS_ARG(verbose, "-v");
+		HAS_ARG(verbose, "--verbose");
+	);
 
 	if (help) {
 		PRINTLN("Usage: minhn [OPTION...]");
@@ -131,19 +146,18 @@ int main(int argc, char *argv[])
 
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 
-	stories_text = request_body(
-		"https://hacker-news.firebaseio.com/v0/topstories.json");
-
-	if (!stories_text) {
+	if (!request_body(
+		    stories_text, HTTP_BUFSIZ,
+		    "https://hacker-news.firebaseio.com/v0/topstories.json")) {
 		FATAL("could not retrieve top stories");
 	}
 
 	stories_root = json_loads(stories_text, 0, &jerror);
-	free(stories_text);
 
 	if (!stories_root) {
 		char *msg = jerror.text;
-		FATAL("failed to parse top stories, %s", msg);
+		FATAL("failed to parse top stories, %s:\n%s", msg,
+		      stories_text);
 	}
 
 	if (!json_is_array(stories_root)) {
@@ -151,7 +165,7 @@ int main(int argc, char *argv[])
 	}
 
 	for (int i = 0; i < NUM_STORIES; ++i) {
-		json_t *element, *story;
+		json_t *element, *story_root;
 		json_int_t id;
 
 		element = json_array_get(stories_root, i);
@@ -162,21 +176,21 @@ int main(int argc, char *argv[])
 
 		id = json_integer_value(element);
 
-		memset(story_text_url, 0, sizeof(story_text_url));
 		sprintf(story_text_url,
 			"https://hacker-news.firebaseio.com/v0/item/%lld.json",
 			id);
 
-		story_text = request_body(story_text_url);
+		if (!request_body(story_text, HTTP_BUFSIZ, story_text_url)) {
+			FATAL("could not retrieve story");
+		}
 
-		story = json_loads(story_text, 0, &jerror);
-		if (!json_is_object(story)) {
+		story_root = json_loads(story_text, 0, &jerror);
+		if (!json_is_object(story_root)) {
 			FATAL("received unknown json story blob, %s:\n%s",
 			      (char *)jerror.text, story_text);
 		}
-		free(story_text);
 
-		stories[i] = story;
+		stories[i] = story_root;
 	}
 
 	/* output length return unused */
